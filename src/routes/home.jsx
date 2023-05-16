3b5a2591ff4dd897bbbc4df42f06922b1e3dc14f
darideveloper <darideveloper@gmail.com>
2023-05-16 15:11:53 -0600

    feat (components): create section-paralax

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,12 +1,19 @@
-import { Outlet, Link } from "react-router-dom"
+// Components
+import { Link } from "react-router-dom"
+import SectionParalax from "../components/section-paralax"
 
 export default function Home() {
+
   return (
     <>
-      <section className="hero w-full relative flex items-center justify-center h-52 sm:h-80 md:h-96 lg:h-screen overflow-hidden bg-fixed bg-center bg-cover bg-[url('./hero.png')]" style={{maxHeight: "400px"}}>
+      <SectionParalax
+        extraClasses="hero flex items-center justify-center"
+        imageUrl="./hero.png">
+
         <Link to={"buy"} className="rounded-xl bg-yellow text-2xl sm:text-3xl md:text-4xl font-bold text-green px-5 sm:px-6 md:px-8 lg:px-10 py-1 sm:py-2 md:py-2 lg:py-3 uppercase shadow-lg border-4 border-yellow duration-300 hover:bg-transparent hover:text-yellow">Comprar boletos</Link>
-      </section>
-      <p style={{height: "10000px"}}></p>
+
+      </SectionParalax>
+      <p style={{ height: "10000px" }}></p>
     </>
   )
 }
