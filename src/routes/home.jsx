ce61c859b24ffa28467536ad8035f87a0e04e1a4
darideveloper <darideveloper@gmail.com>
2023-05-17 01:05:38 -0600

    style (hero): h1 bg color

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -10,13 +10,13 @@ export default function Home() {
   return (
     <>
       <section className="hero">
+        <h1 className="w-full text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold bg-pink py-2">Sorteos Ajolote</h1>
         <Paralax
           extraClasses="flex items-center justify-center relative h-52 sm:h-80 md:h-96 lg:h-screen"
           imageUrl="./hero.jpg"
           maxHeight={400}
         >
           <>
-            <h1 className="absolute -top-3 sm:-top-4 md:-top-5 lg:-top-5 left-0 w-full text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold">Sorteos Ajolote</h1>
             <Link to={"buy"} className="rounded-xl bg-yellow text-2xl sm:text-3xl md:text-4xl font-bold text-green px-5 sm:px-6 md:px-8 lg:px-10 py-1 sm:py-2 md:py-2 lg:py-3 uppercase shadow-lg border-4 border-yellow duration-300 hover:bg-transparent hover:text-yellow">Comprar boletos</Link>
           </>
         </Paralax>
