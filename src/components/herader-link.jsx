6abcc1a8511d43aaf8c11d29b969a856ec81dac5
darideveloper <darideveloper@gmail.com>
2023-05-17 01:20:09 -0600

    style (header): hover styles

--- src/components/herader-link.jsx
+++ src/components/herader-link.jsx
@@ -2,12 +2,15 @@ import PropTypes from 'prop-types'
 import { Link } from "react-router-dom"
 
 export default function HeaderLink({ text, to, isLink=false }) {
+
+  const styles = 'py-3 block sm:px-5 hover:text-yellow duration-300'
+
   return (
     isLink 
     ?
-    <Link to={to} className='py-3 block sm:px-5'>{text}</Link>
+    <Link to={to} className={styles}>{text}</Link>
     : 
-    <a href={to} className='py-3 block sm:px-5'>{text}</a>
+    <a href={to} className={styles}>{text}</a>
   )
 }
 
